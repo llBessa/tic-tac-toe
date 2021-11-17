383b545a75416c969e0919a082cceaf2c439973f
llBessa <lucas.bessafp72@gmail.com>
2021-11-17 17:55:10 -0400

    more changes

---
+++ pages/velha.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="icon" href="../imagens/board.png">
+    <title>Tic Tac Toe</title>
+</head>
+<body>
+    <?php
+        include_once("navigator.php");
+    ?>
+    <br><br><br>
+    <div class="imagem">
+        <img src="../imagens/board.png" alt="jogo da velha" id="board">
+    </div>
+    
+</body>
+</html>
