d654329fbd9c8acede17e2d9f9fb535993ab18e9
Lucas Bessa <lucas.bessafp72@gmail.com>
2022-05-06 09:38:34 -0400

    bootstrap, fontawesome e navbar

--- pages/head.php
+++ pages/head.php
@@ -4,20 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
-    <link rel="icon" href="../images/board.png">
-    <title>Home Page</title>
+    <title>Jogo da Velha</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
 </head>
 <body>
-    <div class="navigator">
-        <div class="logo">
-            <h1 class="title">Tic Tac Toe </h1>
-        </div>
-        <ul>
-            <nav class="list">
-                <strong class="list-title">Serviços:</strong> <br> <br>
-                <li><a href="index.php"> Home</a></li>
-                <li><a href="velha.php"> jogar agora</a></li>
-            </nav>
-        </ul>
-    </div>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a href="#">
+                <div class="navbar-brand">
+                    Jogo da velha
+                </div>
+            </a>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Jogar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Pontuação</a>
+                </li>
+            </ul>
+        </nav>              
+    </header>
+</body>
+</html>
